961d7390122e3c9b2a0da9ebf7558780667c2ad8
Alexander Sidorenko <servicebox.work@yandex.ru>
2025-07-22 00:13:27 +0200

    refactor: Add   type ignore

--- migrations/env.py
+++ migrations/env.py
@@ -83,7 +83,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
-            render_item=render_item,
+            render_item=render_item,   # type: ignore
         )
         with context.begin_transaction():
             context.run_migrations()
